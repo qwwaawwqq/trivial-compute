36ee7d3b53f7db122eb077ac9c11ccaff794659a
Casey Rock <casey.f.rock.civ@army.mil>
2024-07-13 13:48:37 -0400

    rearage public files

--- app.js
+++ app.js
@@ -35,8 +35,7 @@ app.use(function (req, res, next) {
 });
 
 // Serve static files
-app.use('/', express.static(join(__dirname, '')));
-
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`);

66151d96739c619c1c34ff990186c8d5c6db0079
Casey Rock <casey.f.rock.civ@army.mil>
2024-08-04 20:21:47 -0400

    fix routing for the main page, bi-pass auth for minial increment

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,36 @@
-$(document).ready(function() {
-    $('.play-button').on('click', function() {
-        $(this).fadeOut(150).fadeIn(150, function() {
+$(document).ready(function () {
+    $('.play-button').on('click', function () {
+        $(this).fadeOut(150).fadeIn(150, function () {
             window.location.href = './gameConfig.html';
         });
     });
 
-    $('#manage-accounts').on('click', function() {
-        window.location.href = './authenticationAccount.html';
+    $('#manage-accounts').on('click', function () {
+        window.location.href = './manageAccount.html';
     });
 
-    $('#manage-questions').on('click', function() {
-        window.location.href = './authenticationQuestion.html';
+    $('#manage-questions').on('click', function () {
+        window.location.href = './manageQuestion.html';
     });
 
-    $('#manage-categories').on('click', function() {
-        window.location.href = './authenticationCategories.html';
+    $('#manage-categories').on('click', function () {
+        window.location.href = './manageCategories.html';
     });
+
+
+
+
+    // PLAN TO ADD AUTH TO TARGET INCREMENT
+    // $('#manage-accounts').on('click', function() {
+    //     window.location.href = './authenticationAccount.html';
+    // });
+
+    // $('#manage-questions').on('click', function() {
+    //     window.location.href = './authenticationQuestion.html';
+    // });
+
+    // $('#manage-categories').on('click', function() {
+    //     window.location.href = './authenticationCategories.html';
+    // });
 
 });

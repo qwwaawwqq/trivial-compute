4ec50abe31180de0f35f8c0fdb3e8df250bebfa4
Calvin Nguyen <calvinnguyenv@gmail.com>
2024-08-06 07:50:39 -0700

    button fix

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -368,6 +368,17 @@ document.addEventListener('DOMContentLoaded', () => {
         // Add functionality to return to the previous page
     })
 
+    $("#return-button2").click(function() {
+        let consent = confirm("Return to Home?");
+        if (consent) {
+            window.location.href = 'gameConfig.html'
+        }
+        else {
+            console.log("Somebody tried to quit");
+        }
+        // Add functionality to return to the previous page
+    })
+
 
     $('.textq').click(function() {
         let direction = $(this).attr("id");
